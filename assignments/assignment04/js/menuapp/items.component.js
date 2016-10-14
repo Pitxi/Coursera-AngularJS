08e202787b580e008c96b933fb614f07be3dc4af
Javier Campón Pichardo <javiercamponp@gmail.com>
2016-10-14 19:25:26 -0400

    Removed unnecesary controller for items component.

--- assignments/assignment04/js/menuapp/items.component.js
+++ assignments/assignment04/js/menuapp/items.component.js
@@ -4,19 +4,8 @@
 	angular.module('MenuApp')
 	.component('items', {
 		templateUrl:'partials/items.component.template.html',
-		controller: ItemsComponentController,
 		bindings: {
 			itemsList: '<'
 		}
 	});
-
-	function ItemsComponentController() {
-		var ctrl = this;
-
-
-
-		ctrl.$onInit = function() { };
-		ctrl.$onChanges = function(changesObj) { };
-		ctrl.$onDestory = function() { };
-	}
 })();
