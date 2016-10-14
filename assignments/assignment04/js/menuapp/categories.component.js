dbb88157b790f6e88f26338760def7c48c6fa85c
Javier Campón Pichardo <javiercamponp@gmail.com>
2016-10-14 19:21:18 -0400

    Removed unnecessary controller for categories component.

--- assignments/assignment04/js/menuapp/categories.component.js
+++ assignments/assignment04/js/menuapp/categories.component.js
@@ -4,17 +4,8 @@
 	angular.module('MenuApp')
 	.component('categories', {
 		templateUrl:'partials/categories.component.template.html',
-		controller: CategoriesComponentController,
 		bindings: {
 			categoriesList: '<'
 		}
 	});
-
-	function CategoriesComponentController() {
-		var ctrl = this;
-
-		ctrl.$onInit = function() { };
-		ctrl.$onChanges = function(changesObj) { };
-		ctrl.$onDestory = function() { };
-	}
 })();
